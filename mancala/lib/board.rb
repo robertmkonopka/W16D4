6cb2f4d3bed6d9aa8da606f9098f43c4a7cc9ed9
robertk <robertmakoto@mac.com>
2020-07-01 23:55:05 -0700

    Added 'initialize' method to 'Board' class

--- mancala/lib/board.rb
+++ mancala/lib/board.rb
@@ -2,6 +2,9 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1, @name2 = name1, name2
+    @cups = Array.new(14) {Array.new}
+    place_stones
   end
 
   def place_stones
